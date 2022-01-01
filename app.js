bdf66b67ac0e5434bba3edf05df59349d756fcf5
Vladislav Kim <vladkim165@gmail.com>
2022-01-02 00:11:17 +0300

    initial pull request

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+require('dotenv').config();
+const { login, logout } = require('./controllers/login');
+const { createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const allowedOrigins = require('./middlewares/cors');
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(cookieParser());
+app.use(requestLogger);
+
+app.use(allowedOrigins);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signout', logout);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+
+app.use(auth);
+app.use('/users', require('./routes/users'));
+app.use('/movies', require('./routes/movies'));
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.get('*', (_req, _res, next) => {
+  const err = new Error('Запрашиваемый ресурс не найден');
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(PORT);
