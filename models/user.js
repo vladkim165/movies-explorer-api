1419dfb7ca75c55ed6f68472e92a22b896eb946a
Vladislav Kim <vladkim165@gmail.com>
2022-01-04 16:35:26 +0300

    fixed/validation, error-status, env

--- models/user.js
+++ models/user.js
@@ -24,17 +24,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = (email, password) => this.findOne({ email }).select('+password').then((user) => {
-  if (!user) {
-    return Promise.reject(new Error('Неправильные почта или пароль'));
-  }
-  return bcrypt.compare(password, user.password)
-    .then((matched) => {
-      if (!matched) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
-      }
-      return user;
-    });
-});
+userSchema.statics.findUserByCredentials = (email, password) => {
+  this.findOne({ email }).select('+password').then((user) => {
+    if (!user) {
+      return Promise.reject(new Error('Неправильные почта или пароль'));
+    }
+    return bcrypt.compare(password, user.password)
+      .then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error('Неправильные почта или пароль'));
+        }
+        return user;
+      });
+  });
+};
 
 module.exports = mongoose.model('user', userSchema);
