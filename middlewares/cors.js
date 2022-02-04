a4acdecfc80fdbf5fd0e8183df668f7d0cd75152
Vladislav Kim <vladkim165@gmail.com>
2022-02-04 23:52:41 +0300

    fixed/routes

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,7 +9,7 @@
 module.exports = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
   // if (allowedCors.includes(origin)) {
   //   res.header('Access-Control-Allow-Origin', origin);
   // }
