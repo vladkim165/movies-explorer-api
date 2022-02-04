25daf50162c899213db59b587399d41d961cdbcb
Vladislav Kim <vladkim165@gmail.com>
2022-02-04 22:00:38 +0300

    added/login after successful register

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,14 +9,19 @@
 module.exports = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials');
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   // if (allowedCors.includes(origin)) {
   //   res.header('Access-Control-Allow-Origin', origin);
   // }
 
+  res.header('Access-Control-Allow-Credentials', true);
+
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+
     const requestHeaders = req.headers['access-control-request-headers'];
     res.header('Access-Control-Allow-Methods', requestHeaders);
+
     // удалить после разработки
     res.header('Access-Control-Allow-Origin', origin);
 
