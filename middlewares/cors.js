c49556d8bfded2313b4407f3b403f49cd77d1346
Vladislav Kim <vladkim165@gmail.com>
2022-02-04 21:42:53 +0300

    added/login after successful register

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,30 +1,25 @@
-// const allowedCors = [
-//   'https://movies-explorer-prod.nomoredomains.rocks/',
-//   'http://movies-explorer-prod.nomoredomains.rocks/',
-//   'https://api.movies-explorer-prod.nomoredomains.rocks/',
-//   'http://api.movies-explorer-prod.nomoredomains.rocks/',
-//   'http://localhost/3000'
-//   'http://localhost',
-// ];
+const allowedCors = [
+  'https://movies-explorer-prod.nomoredomains.rocks/',
+  'http://movies-explorer-prod.nomoredomains.rocks/',
+  'https://api.movies-explorer-prod.nomoredomains.rocks/',
+  'http://api.movies-explorer-prod.nomoredomains.rocks/',
+  'http://localhost/3000',
+];
 
 module.exports = (req, res, next) => {
   const { method } = req;
+  const { origin } = req.headers;
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials');
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   if (method === 'OPTIONS') {
     const requestHeaders = req.headers['access-control-request-headers'];
     res.header('Access-Control-Allow-Methods', requestHeaders);
-    const { origin } = req.headers;
-    res.header('Access-Control-Allow-Origin', origin);
 
     return res.end();
   }
-
-  const { origin } = req.headers;
-  res.header('Access-Control-Allow-Origin', origin);
-  // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  // }
 
   return next();
 };
