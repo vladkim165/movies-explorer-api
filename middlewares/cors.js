40222b90b997aa74d2d5049df03168a3afe2dbfd
Vladislav Kim <vladkim165@gmail.com>
2022-02-05 00:39:29 +0300

    fixed/cors headers

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,20 +6,20 @@
 //   'http://localhost/3000',
 // ];
 
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
+
 module.exports = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
   // if (allowedCors.includes(origin)) {
   //   res.header('Access-Control-Allow-Origin', origin);
   // }
 
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials');
 
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', requestHeaders);
-
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials');
     // удалить после разработки
     res.header('Access-Control-Allow-Origin', origin);
 
