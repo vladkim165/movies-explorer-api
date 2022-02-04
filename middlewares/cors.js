002306b9d478e84a79add0b0513118b901697a05
Vladislav Kim <vladkim165@gmail.com>
2022-02-04 23:54:23 +0300

    fixed/routes

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,16 +9,15 @@
 module.exports = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
-  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
   // if (allowedCors.includes(origin)) {
   //   res.header('Access-Control-Allow-Origin', origin);
   // }
 
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials');
-  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+
   if (method === 'OPTIONS') {
-    const requestHeaders = req.headers['access-control-request-headers'];
     res.header('Access-Control-Allow-Methods', requestHeaders);
 
     // удалить после разработки
